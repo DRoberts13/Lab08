ba9089647ccda5d744e0e822273d0cabb0727a38
DRoberts13 <123511055+DRoberts13@users.noreply.github.com>
2023-03-26 00:06:52 -0400

    Update Lab8.py

--- Lab8.py
+++ Lab8.py
@@ -1,3 +1,7 @@
-userName = input ("Hello! Please enter your name! ")
+userName = input("Hello! Please enter your facebook username! ")
+userPassword = input("Hello! Please enter your facebook password! ")
 
-print ("Hello", userName, "nice to meet you!")
+    if(userName.lower() == "derrick" and userPassword == "programing"):
+        print("Welcome to facebook!")
+    else:
+        print("Wrong credentials, try again!")
